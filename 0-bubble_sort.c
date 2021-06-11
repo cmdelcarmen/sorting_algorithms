a4600ba3479ab4dbb5a38eaeafc7ec53c4bbab9d
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-11 01:54:15 +0000

    first commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,64 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function sorts array using bubble sort
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int stop = 0;
+
+	while (stop == 0)
+	{
+		stop = check_if_sorted(array, size);
+		if (stop == 0)
+			bubble_sort_array(array, size);
+	}
+}
+
+/**
+ * check_if_sorted - function checks if array is sorted
+ * @array: array in need to be sorted
+ * @size: size of array
+ * Return: 1 if array is sorted, 0 if array not sorted
+ */
+int check_if_sorted(int *array, size_t size)
+{
+	unsigned int element, stop = 0;
+
+	for (element = 0; element < size; element++)
+	{
+		if ((element + 1) < size)
+		{
+			if (array[element] > array[element + 1])
+				return (stop);
+		}
+	}
+	stop = 1;
+	return (stop);
+}
+
+/**
+ * bubble_sort_array - function bubble sorts array
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void bubble_sort_array(int *array, size_t size)
+{
+	unsigned int element, element_temp;
+
+	for (element = 0; element < size; element++)
+	{
+		if ((element + 1) < size)
+		{
+			if (array[element] > array[element + 1])
+			{
+				element_temp = array[element];
+				array[element] = array[element + 1];
+				array[element + 1] = element_temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ sort.h
@@ -0,0 +1,39 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+/*file_name*/
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/*file name: 0-bubble_sort.c*/
+void bubble_sort(int *array, size_t size);
+void bubble_sort_array(int *array, size_t size);
+int check_if_sorted(int *array, size_t size);
+
+
+/*file_name*/
+void insertion_sort_list(listint_t **list);
+
+/*file_name*/
+void insertion_sort_list(listint_t **list);
+
+
+#endif
