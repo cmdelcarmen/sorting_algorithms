ccc56298194f9d9123b57fde408d8966e67f5eb5
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-13 21:20:34 +0000

    numer 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,13 +18,37 @@ void selection_sort(int *array, size_t size)
 }
 
 /**
+ * check_if_sorted - function checks if array is sorted
+ * @array: array in need to be sorted
+ * @size: size of array
+ * Return: 1 if array is sorted, 0 if array not sorted
+ */
+int check_if_sorted(int *array, size_t size)
+{
+	unsigned int element, stop = 0;
+
+	for (element = 0; element < size; element++)
+	{
+		if ((element + 1) < size)
+		{
+			if (array[element] > array[element + 1])
+				return (stop);
+		}
+	}
+	stop = 1;
+	return (stop);
+}
+
+
+/**
  * selection_sort_array - function sorts array using selection sort
  * @array: size of array
  * @size: size of array
  */
 void selection_sort_array(int *array, size_t size)
 {
-	int idx, idx2, min_num, number_holder, swap_idx, swap = 0;
+	unsigned int idx, idx2, number_holder, swap_idx, swap = 0;
+	int min_num;
 
 	for (idx = 0; idx < size; idx++)
 	{
